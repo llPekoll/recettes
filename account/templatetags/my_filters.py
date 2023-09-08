d48ada0ab58608a29c1ce8f22ceba5c1dd9c3371
peko <yoyo.mepa@gmail.com>
2023-09-08 13:54:37 +0200

    added filter,css edit navbar

--- account/templatetags/my_filters.py
+++ account/templatetags/my_filters.py
@@ -1,4 +1,5 @@
 from django import template
+from django.utils.translation import get_language_info
 
 register = template.Library()
 
@@ -6,3 +7,8 @@ register = template.Library()
 @register.filter
 def clean_label_for_url(value):
     return value.replace(" ", "-").lower()
+
+
+@register.filter
+def language_name(code):
+    return get_language_info(code)["name"]

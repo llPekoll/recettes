58b0f1281117300ff8684bd3feffe62558659cc8
peko <yoyo.mepa@gmail.com>
2024-01-03 00:32:20 +0100

    fix bugs on delete items

--- playwright_tests/lib/report.py
+++ playwright_tests/lib/report.py
@@ -5,4 +5,5 @@ def post_report(page: Page) -> None:
     page.locator('[data-test="report"]').click()
     time.sleep(1)
     page.locator(".swal2-confirm").click()
+    page.locator(".swal2-confirm").click()
 
